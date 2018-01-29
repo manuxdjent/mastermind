2d9f41a222d8c59ad49bedb50274db944565ef03
manuxdjent <manuxdjent@gmail.com>
2018-01-29 14:45:52 +0100

    .

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
   var jugador = [];
   var blancos = 0, negros = 0;
   var puntuacion = "";
+  var hanSalido = [];
+  var intentos = 5;
+
   Array.prototype.unique = function() {
     return this.filter(function (value, index, self) {
     return self.indexOf(value) === index;
@@ -20,9 +23,9 @@ $(document).ready(function(){
   function comprobarBlancos(){
     var combinacionUnique = combinacion.unique();
     var jugadorUnique = jugador.unique();
-    alert("Combinacion = "+combinacionUnique+" Jugador = "+jugadorUnique);
-    for (var i = 0; i < combinacionUnique.length; i++) {
-      if (jugadorUnique.indexOf(combinacionUnique[i]) != -1) {
+    //alert("Combinacion = "+combinacionUnique+" Jugador = "+jugadorUnique);
+    for (var i = 0; i < jugadorUnique.length; i++) {
+      if (combinacionUnique.indexOf(combinacionUnique[i]) != -1) {
         blancos++;
       }
     }
@@ -42,13 +45,22 @@ $(document).ready(function(){
     blancos = 0;
     puntuacion = "";
   }
-  for (var i = 0; i < 4; i++) {
-    var n = Math.floor((Math.random() * 4) + 1);
-    combinacion.push(n);
-  }
+  for (var i = 1; i <= 4; i++) {
+          var azar;
+          do {
+            azar = Math.floor(Math.random() * 4 + 1);
+          } while (hanSalido.indexOf(azar) > -1);
+          hanSalido.push(azar);
+          combinacion.push(azar);
+        }
   alert("Combinación = "+combinacion);
   $(".opciones").children()
   .mouseup(function(){
+    if (intentos == 0) {
+      html = "<div>Has agotado los intentos, el código era "+combinacion+"</div>";
+    }else if (combinacion == jugador) {
+      html = "<div> Has acertado el código </div>";
+    } else{
       html += "<div class='"+$(this).attr('class')+"' name='"+$(this).attr('name')+"'/>";
       //alert(html);
       jugador.push($(this).attr('name'));
@@ -56,11 +68,12 @@ $(document).ready(function(){
       if(a == 4){
         comprobarNegros();
         comprobarBlancos();
-        alert("Jugador = "+jugador+" Negros = "+negros + "Blancos = "+blancos);
+        alert("Combinacion: "+ combinacion + " Jugador = "+jugador+" Negros = "+negros + "Blancos = "+blancos);
         sacarNegrosBlancos();
         $("h1").after(html+"<div class='p'>"+puntuacion+"</div></div>");
         html = "<div class='resultado'>";
         variablesReset();
       }
+    }
   });
 });
